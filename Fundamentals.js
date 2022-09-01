4c3fca6c2a2b820aa6a43497bfab0a4fdd8ae24f
LA Beesley <97586702+labeesley@users.noreply.github.com>
2022-09-01 16:15:02 -0400

    Create Fundamentals.js

---
+++ Fundamentals.js
@@ -0,0 +1,12 @@
+//let's practice the fundamentals of javascript
+
+////////\\\\\\\\
+/////Sept 1\\\\\
+////////\\\\\\\\
+
+// Function 3 - multiplying two numbers
+// Implement a function called multiply, which takes two numbers and returns their product:
+
+// multiply(2, 3) = 6
+// multiply(0, 188) = 0
+// multiply(85, 144) = 12240
